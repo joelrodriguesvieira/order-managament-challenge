6b65fda51b21152511923b1ac66726d5289de096
Joel Rodrigues Vieira <joel2j39@gmail.com>
2026-01-02 20:14:25 -0300

    feat: add validation with zod in register and login route

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,14 +3,28 @@ import { AuthService } from './auth.service';
 import { LoginDTO, RegisterDTO } from './auth.types';
 import { ErrorHandler } from '../../shared/errors/error';
 import { HttpErrorsStatusCode } from '../../shared/errors/error.types';
+import { loginSchema, registerSchema } from './auth.schema';
+import z, { ZodError } from 'zod';
+import { createErrorMap, fromError } from 'zod-validation-error';
+
+z.config({
+  customError: createErrorMap(),
+});
 
 export class AuthController {
   static async register(req: Request, res: Response) {
     try {
-      const { email, password }: RegisterDTO = req.body;
+      const data = registerSchema.parse(req.body);
+      const { email, password }: RegisterDTO = data;
       const result = await AuthService.register({ email, password });
       return res.status(201).json(result);
     } catch (error) {
+      if (error instanceof ZodError) {
+        const validationError = fromError(error);
+        return res.status(HttpErrorsStatusCode.BAD_REQUEST).json({
+          message: validationError.toString(),
+        });
+      }
       if (error instanceof ErrorHandler) {
         return res.status(error.statusCode).json({ message: error.message });
       }
@@ -22,10 +36,17 @@ export class AuthController {
 
   static async login(req: Request, res: Response) {
     try {
-      const { email, password }: LoginDTO = req.body;
+      const data = loginSchema.parse(req.body);
+      const { email, password }: LoginDTO = data;
       const result = await AuthService.login({ email, password });
       return res.json(result);
     } catch (error) {
+      if (error instanceof ZodError) {
+        const validationError = fromError(error);
+        return res.status(HttpErrorsStatusCode.BAD_REQUEST).json({
+          message: validationError.toString(),
+        });
+      }
       if (error instanceof ErrorHandler) {
         return res.status(error.statusCode).json({ message: error.message });
       }
---
+++ src/modules/auth/auth.schema.ts
@@ -0,0 +1,13 @@
+import z from 'zod';
+
+export const registerSchema = z.object({
+  email: z.email().nonempty({ message: 'Email is required' }),
+  password: z
+    .string()
+    .min(6, { message: 'Password must be at least 6 characters long' }),
+});
+
+export const loginSchema = z.object({
+  email: z.email().nonempty({ message: 'Email is required' }),
+  password: z.string().nonempty({ message: 'Password is required' }),
+});
